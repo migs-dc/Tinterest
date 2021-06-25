e4a07279997e27a761a901bca3305d6e9c991a3f
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-06-25 09:12:05 -0400

    added image url to pins table

--- tinterest/db/schema.rb
+++ tinterest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_120749) do
+ActiveRecord::Schema.define(version: 2021_06_25_104452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_06_24_120749) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url", null: false
     t.index ["title"], name: "index_pins_on_title"
     t.index ["user_id"], name: "index_pins_on_user_id"
   end
