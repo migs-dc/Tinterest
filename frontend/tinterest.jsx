ddf1d144e6823a473ce9a17a359cf91b071b78c9
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-06 07:01:27 -0400

    SessionApiUtil

--- frontend/tinterest.jsx
+++ frontend/tinterest.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
+
+import { signin, login, logout } from './util/session_api_util'
+
+window.signin = signin;
+window.login = login;
+window.logout = logout;
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const store = configureStore();
+  const store = configureStore();
   ReactDOM.render(<Root />, root);
-  // ReactDOM.render(<h1>hello</h1>, root);
 })
