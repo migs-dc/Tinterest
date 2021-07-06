545f79be4cb9585933bdbbe9c9762d75cf1934d7
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-06 17:55:05 -0400

    preloaded state, logged in user stays logged in after refresh

--- frontend/tinterest.jsx
+++ frontend/tinterest.jsx
@@ -6,14 +6,27 @@ import configureStore from './store/store';
 import { signup, login, logout } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      session: { id: window.currentUser.id },
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
+
   window.signup = signup;
   window.login = login;
   window.logout = logout;
-
-  const store = configureStore();
-    window.getState = store.getState;
-    window.dispatch = store.dispatch;
-
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
+  // const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 })
