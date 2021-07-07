28e4b4797db069708e108e1f4bb06cfb60cf0b1a
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-06 21:12:07 -0400

    finished limiting what logged in users can see. Doing some styling now

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import GreetingContainer from "./greeting/greeting_container";
 import LoginFormContainer from './session_form/login_form_container'
 import SignupFormContainer from './session_form/signup_form_container'
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-    <header>
-      <h1>Tinterest</h1>
+    <header className="top-nav">
+      <Link to="/" className="header-link">
+        <h1>Tinterest</h1>
+      </Link>
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
-    
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>    
   </div>
 );
 
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Greeting = ({ currentUser, logout }) => {
   const sessionLinks = () => (
-    <nav className="login-signup">
-      <Link to="/login">Login</Link>
-      <Link to="/signup">Sign up!</Link>
-    </nav>
+    <div className="login-signup">
+      <button><Link to="/login">Log in</Link></button>
+      <button><Link to="/signup">Sign up</Link></button>
+    </div>     
   );
 
   const personalGreeting = () => (
