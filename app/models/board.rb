2a9dae1a49c128e04e35ef650ee7ba462deeee18
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-12 17:52:46 -0400

    finished my model associations

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,16 +1,18 @@
 class Board < ApplicationRecord
   validates :title, :user_id, presence: true
 
-  belongs_to :user
+  belongs_to :user,
     primary_key: :id,
     foreign_key: :user_id,
     class_name: :User
     
-  has_many :pins
+  has_many :saves,
     primary_key: :id,
     foreign_key: :board_id,
     class_name: :PinBoard
 
-  
-    
+  has_many :pins,
+    through: :saves,
+    source: :pin
+
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,17 @@
 class Pin < ApplicationRecord 
   validates :title, :image_url, :user_id, presence: true
-  belongs_to :user  
+
+  belongs_to :user
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: :User
+
+  has_many :saves,  
+    primary_key: :id,
+    foreign_key: :pin_id,
+    class_name: :PinBoard
+
+  has_many :boards,
+    through: :saves,
+    source: :board
 end
