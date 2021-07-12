90299c0ace16e5dcd71ad167d2bd853c09904744
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-07-12 17:53:56 -0400

    merging

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   after_initialize :ensure_session_token
 
   has_many :pins
+  has_many :boards
 
   def self.find_by_credentials(email, password)
     user = User.find_by(email: email)
