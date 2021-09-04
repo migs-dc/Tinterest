6c7d436ba2d35738d47ea292cedf329588fd7bef
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-09-03 23:27:42 -0400

    follows container

--- frontend/components/follows/follows_container.js
+++ frontend/components/follows/follows_container.js
@@ -1,1 +1,12 @@
-import { connect }
+import { connect } from 'react-redux'
+import follows from './follow'
+
+const mSTP = (state) => {
+
+}
+
+const mDTP = dispatch => ({
+  
+})
+
+export default connect(mSTP, mDTP)(follows);
