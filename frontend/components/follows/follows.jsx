b3cc549519f84b82e8dd2cb8d19dbdef2a91d874
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-09-03 23:16:05 -0400

    follows.js

---
+++ frontend/components/follows/follows.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class follows extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    return null;
+    // return(
+
+    // )
+  }
+}
