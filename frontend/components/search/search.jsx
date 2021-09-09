5bdef49eee25d1f47094bd37ca0988cb7d110559
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-09-08 23:31:48 -0400

    search

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,9 +4,7 @@ const Search = (props) => {
 
 
   // return(
-    // been mostly doing bug fixes and visual changes
-    // haven't really been doing my mvps at all
-    // really should wrap this up so i can start applying and preparing for exams
+    
   // )
 
   return null;
