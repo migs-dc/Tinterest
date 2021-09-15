1de8f1dc56174b99561272b9759aa29ad11a1af6
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-09-14 23:14:00 -0400

    search form

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -4,11 +4,27 @@ import { Link } from "react-router-dom";
 const Search = (props) => {
 
 
-  // return(
-  
-  // )
-
-  return null;
+  return(
+    <div key={Math.random()}>
+        <div className='search'>  
+            <form className='search-form'>
+                <div className='search-input'>
+                    <input type="text" placeholder="Search for a Pin or a Board" value={search} onChange={
+                        e => setSearch(e.target.value)
+                    }/>
+                </div> 
+                <div className="search-button">
+                    <Link to = '/pins'>
+                        <button type='submit' className='search-button'>
+                            <i className="fas fa-search"></i>
+                        </button>
+                    </Link>
+                </div>
+            </form>
+            </div>
+            
+    </div>
+  )
 }
 
 export default Search;
