ebdd89c44fcc5e76e1bae893f6c19a94eb615094
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-09-29 23:35:30 -0400

    follows.jsx

--- frontend/components/follows/follows.jsx
+++ frontend/components/follows/follows.jsx
@@ -8,30 +8,7 @@ class follows extends React.Component {
   render(){
     
     return(
-      <div className="board-index">
-          <div className="dp">
-          {
-            this.props.currentUser.displayPicture 
-            ? <img src={this.props.displayPicture} className="nav-img"/>
-            : <i className="far fa-user"></i>
-          }
-          </div>
-          <h1>{this.props.user.username}</h1>
-          <br />
-          <Link to='/create-board'><button className="cb-button">Create Board</button></Link>
-          <button className="follow-button">Follow</button>
-          <div className="board-index-box">
-            {this.props.boards.map(board => {
-              if (board.user_id === this.props.user.id) {
-                return <BoardsIndexItem 
-                  user={this.props.user}
-                  board={board} 
-                  key={board.id} />              
-              } 
-            })
-            }
-          </div>
-        </div>
+
 
     )
   }
