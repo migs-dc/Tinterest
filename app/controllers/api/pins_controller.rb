9e349f4d7eab02a610eff5073abfa972d7ab5f95
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-10-14 22:57:32 -0400

    pin controller

--- app/controllers/api/pins_controller.rb
+++ app/controllers/api/pins_controller.rb
@@ -2,11 +2,11 @@ class Api::PinsController < ApplicationController
   before_action :require_logged_in, only: [:create, :edit, :update, :destroy]
 
   def index
-    @pins = Pin.all
+    # @pins = Pin.all
     @pins = bounds ? Pin.in_bounds(param[:bounds]) : Pin.all
     
     render :index
-    render 'api/pins/index'
+    # render 'api/pins/index'
   end
 
   def show
