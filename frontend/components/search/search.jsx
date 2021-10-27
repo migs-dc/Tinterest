81457a9c668a7a65e0ba26ac7f9f7c8609a88cdc
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-10-26 23:40:05 -0400

    change pin index in search.jsx

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import pin_index from "../pin/pins_index"
+// import pin_index from "../pin/pins_index"
 
 const Search = (props) => {
 
