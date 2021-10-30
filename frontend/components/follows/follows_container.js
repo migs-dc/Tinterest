87bc00fe9d855c22c32b0acb2fe36bd9485b0bd0
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-10-29 23:24:56 -0400

    fix mSTP in follows container

--- frontend/components/follows/follows_container.js
+++ frontend/components/follows/follows_container.js
@@ -3,12 +3,12 @@ import follows from './follow'
 
 const mSTP = (state) => {
   return {
-    
-  }
+    currentUser: entities.users[session.id]
+  };
 }
 
 const mDTP = dispatch => ({
-  
+  logout: () => dispatch(logout())
 })
 
 export default connect(mSTP, mDTP)(follows);
