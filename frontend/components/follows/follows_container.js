a84266865b8fc556e36dc34fbdf2f0cf745fd6fb
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-11-01 19:50:51 -0400

    follows

--- frontend/components/follows/follows_container.js
+++ frontend/components/follows/follows_container.js
@@ -2,13 +2,11 @@ import { connect } from 'react-redux'
 import follows from './follow'
 
 const mSTP = (state) => {
-  return {
-    currentUser: entities.users[session.id]
-  };
+  
 }
 
 const mDTP = dispatch => ({
-  logout: () => dispatch(logout())
+
 })
 
 export default connect(mSTP, mDTP)(follows);
