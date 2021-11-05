62749ea8369dd3b4a344d64b7ea26257b6629257
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-11-04 23:12:13 -0400

    follows container error

--- frontend/components/follows/follows_container.js
+++ frontend/components/follows/follows_container.js
@@ -2,8 +2,8 @@ import { connect } from 'react-redux'
 import follows from './follow'
 
 const mSTP = (state) => {
-  const boardId;
-  const userId;
+  const boardId = parseInt(match.params.boardId);
+  const userId = parseInt(match.params.userId);
 }
 
 const mDTP = dispatch => ({
