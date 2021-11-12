99f6577317f9626695387b059ca12c8aac475a91
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-11-11 23:47:06 -0500

    add filters

--- frontend/util/filter_api_util.js
+++ frontend/util/filter_api_util.js
@@ -1,3 +1,23 @@
 class filterUtil {
-  constructor()
+  constructor(map, handleClick){
+    this.map = map;
+    this.handleClick = handleClick;
+    this.markers = {};
+  }
+
+  updateMarkers(benches){
+    const benchesObj = {};
+    benches.forEach(bench => benchesObj[bench.id] = bench);
+
+    benches
+      .filter(bench => !this.markers[bench.id])
+      .forEach(newBench => this.createMarkerFromBench(newBench, this.handleClick))
+
+    Object.keys(this.markers)
+      .filter(benchId => !benchesObj[benchId])
+      .forEach((benchId) => this.removeMarker(this.markers[benchId]))
+  }
+
 }
+
+export default filterUtil;
